0d76ed9b8b78f4650fd9314cc1517d7b587cc3ee
kingbri <bdashore3@proton.me>
2023-12-19 00:01:27 -0500

    Revert "Start: Add an argument parser to batch file"
    This reverts commit 097c298c397a58da108472b663cf65fe35662b2a.

--- Start.bat
+++ Start.bat
@@ -1,25 +1,13 @@
 @echo off
 
 set VENV_DIR=
-:: Requirements file to use. Defaults to nowheel to avoid mis-installation of dependencies
 set REQUIREMENTS_FILE=
 
 if not defined PYTHON (set PYTHON=python)
 if not defined VENV_DIR (set "VENV_DIR=%~dp0%venv")
-:: Doesn't update wheels by default
 if not defined REQUIREMENTS_FILE (set "REQUIREMENTS_FILE=requirements-nowheel.txt")
-
-if not exist %VENV_DIR%\ (
-    echo "Please create a venv and install dependencies before starting TabbyAPI! Exiting..."
-    exit
-)
-
-:: Argument parsing
-for %%A in (%*) do (
-    if %%A == "--ignore-upgrade" set IGNORE_UPGRADE=y
-)
 
 call "%VENV_DIR%\Scripts\activate.bat"
 call pip -V
-if not defined IGNORE_UPGRADE call pip install --upgrade -r %REQUIREMENTS_FILE%
+if NOT [%1] == [--ignore-upgrade] call pip install --upgrade -r %REQUIREMENTS_FILE%
 call python main.py
