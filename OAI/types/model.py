7bd2506d6b00451b591102d2d5e1098d616b03df
AlpinDale <alpindale@gmail.com>
2023-12-21 12:17:16 +0000

    add pylintrc and fix model.py

--- OAI/types/model.py
+++ OAI/types/model.py
@@ -1,9 +1,13 @@
-from pydantic import BaseModel, Field, ConfigDict
+""" Contains model card types. """
 from time import time
 from typing import List, Optional
+
+from pydantic import BaseModel, Field, ConfigDict
+
 from gen_logging import LogConfig
 
 class ModelCardParameters(BaseModel):
+    """ Represents model card parameters. """
     # Safe to do this since it's guaranteed to fetch a max seq len from model_container
     max_seq_len: Optional[int] = None
     rope_scale: Optional[float] = 1.0
@@ -14,6 +18,7 @@ class ModelCardParameters(BaseModel):
     draft: Optional['ModelCard'] = None
 
 class ModelCard(BaseModel):
+    """ Represents a single model card. """
     id: str = "test"
     object: str = "model"
     created: int = Field(default_factory=lambda: int(time()))
@@ -22,25 +27,37 @@ class ModelCard(BaseModel):
     parameters: Optional[ModelCardParameters] = None
 
 class ModelList(BaseModel):
+    """ Represents a list of model cards. """
     object: str = "list"
     data: List[ModelCard] = Field(default_factory=list)
 
 class DraftModelLoadRequest(BaseModel):
+    """ Represents a draft model load request. """
     draft_model_name: str
     draft_rope_scale: Optional[float] = 1.0
-    draft_rope_alpha: Optional[float] = Field(description = "Automatically calculated if not present", default = None)
+    draft_rope_alpha: Optional[float] = Field(
+        description = "Automatically calculated if not present",
+        default = None)
 
 # TODO: Unify this with ModelCardParams
 class ModelLoadRequest(BaseModel):
+    """ Represents a model load request. """
     name: str
 
     # Max seq len is fetched from config.json of the model by default
-    max_seq_len: Optional[int] = Field(description = "Leave this blank to use the model's base sequence length", default = None)
-    override_base_seq_len: Optional[int] = Field(description = "Overrides the model's base sequence length. Leave blank if unsure", default = None)
+    max_seq_len: Optional[int] = Field(
+        description="Leave this blank to use the model's base sequence length",
+        default=None)
+    override_base_seq_len: Optional[int] = Field(
+        description=("Overrides the model's base sequence length. "
+                     "Leave blank if unsure"),
+        default=None)
     gpu_split_auto: Optional[bool] = True
     gpu_split: Optional[List[float]] = Field(default_factory=list)
     rope_scale: Optional[float] = 1.0
-    rope_alpha: Optional[float] = Field(description = "Automatically calculated if not present", default = None)
+    rope_alpha: Optional[float] = Field(
+        description="Automatically calculated if not present",
+        default=None)
     no_flash_attention: Optional[bool] = False
     # low_mem: Optional[bool] = False
     cache_mode: Optional[str] = "FP16"
@@ -49,6 +66,7 @@ class ModelLoadRequest(BaseModel):
     draft: Optional[DraftModelLoadRequest] = None
 
 class ModelLoadResponse(BaseModel):
+    """ Represents a model load response. """
     # Avoids pydantic namespace warning
     model_config = ConfigDict(protected_namespaces = [])
 
