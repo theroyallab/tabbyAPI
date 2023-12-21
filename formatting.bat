d1aeee66fa7ce95f6e6e526ae5103d9f2cff793b
kingbri <bdashore3@proton.me>
2023-12-21 18:37:54 -0500

    Formatting + Linting: Switch scripts to use ruff
    Also remove the file and recent file change functions from both
    scripts.
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- formatting.bat
+++ formatting.bat
@@ -1,34 +1,20 @@
 @echo off
 setlocal
 
-REM Change to script's directory
+::Change to script's directory
 cd /d %~dp0
 
-REM Get tool versions
-for /f "tokens=2" %%i in ('yapf --version') do set YAPF_VERSION=%%i
-for /f "tokens=2" %%i in ('pylint --version') do set PYLINT_VERSION=%%i
-for /f "tokens=2" %%i in ('mypy --version') do set MYPY_VERSION=%%i
+::Get tool versions
+for /f "tokens=2" %%i in ('ruff --version') do set RUFF_VERSION="%%i"
 
-REM Check tool versions
-call :tool_version_check "yapf" %YAPF_VERSION% "required_version"
-call :tool_version_check "pylint" %PYLINT_VERSION% "required_version"
-call :tool_version_check "mypy" %MYPY_VERSION% "required_version"
+::Check tool versions
+call :tool_version_check "ruff" %RUFF_VERSION% "0.1.9"
 
-REM Format files
-if "%1"=="--files" (
-    shift
-    yapf --in-place --recursive --parallel %*
-) else if "%1"=="--all" (
-    for /r %%i in (*.py) do yapf --in-place --recursive --parallel "%%i"
-) else (
-    REM Format only the files that changed in last commit.
-    REM This part is tricky in Batch and might require additional tools like Git for Windows.
-)
+::Format and lint files
+call ruff format
+call ruff check
 
-REM Run Pylint
-for /r %%i in (*.py) do pylint "%%i"
-
-REM Check if any files were changed
+::Check if any files were changed
 git diff --quiet
 if errorlevel 1 (
     echo Reformatted files. Please review and stage the changes.
--- formatting.sh
+++ formatting.sh
@@ -21,11 +21,9 @@ builtin cd "$(dirname "${BASH_SOURCE:-$0}")"
 ROOT="$(git rev-parse --show-toplevel)"
 builtin cd "$ROOT" || exit 1
 
-YAPF_VERSION=$(yapf --version | awk '{print $2}')
 RUFF_VERSION=$(ruff --version | head -n 1 | awk '{print $2}')
-MYPY_VERSION=$(mypy --version | awk '{print $2}')
 
-# # params: tool name, tool version, required version
+# params: tool name, tool version, required version
 tool_version_check() {
     if [[ $2 != $3 ]]; then
         echo "Wrong $1 version installed: $3 is required, not $2."
@@ -33,71 +31,14 @@ tool_version_check() {
     fi
 }
 
-tool_version_check "yapf" $YAPF_VERSION "$(grep yapf requirements-dev.txt | cut -d'=' -f3)"
 tool_version_check "ruff" $RUFF_VERSION "$(grep "ruff==" requirements-dev.txt | cut -d'=' -f3)"
-tool_version_check "mypy" "$MYPY_VERSION" "$(grep mypy requirements-dev.txt | cut -d'=' -f3)"
 
-YAPF_FLAGS=(
-    '--recursive'
-    '--parallel'
-)
-
-YAPF_EXCLUDES=(
-    '--exclude' 'build/**'
-)
-
-# Format specified files
-format() {
-    yapf --in-place "${YAPF_FLAGS[@]}" "$@"
+# Format and lint all files
+format_and_lint() {
+    ruff format
+    ruff check
 }
 
-# Format files that differ from main branch. Ignores dirs that are not slated
-# for autoformat yet.
-format_changed() {
-    # The `if` guard ensures that the list of filenames is not empty, which
-    # could cause yapf to receive 0 positional arguments, making it hang
-    # waiting for STDIN.
-    #
-    # `diff-filter=ACM` and $MERGEBASE is to ensure we only format files that
-    # exist on both branches.
-    MERGEBASE="$(git merge-base origin/main HEAD)"
-
-    if ! git diff --diff-filter=ACM --quiet --exit-code "$MERGEBASE" -- '*.py' '*.pyi' &>/dev/null; then
-        git diff --name-only --diff-filter=ACM "$MERGEBASE" -- '*.py' '*.pyi' | xargs -P 5 \
-             yapf --in-place "${YAPF_EXCLUDES[@]}" "${YAPF_FLAGS[@]}"
-    fi
-
-}
-
-# Format all files
-format_all() {
-    yapf --in-place "${YAPF_FLAGS[@]}" "${YAPF_EXCLUDES[@]}" *.py
-}
-
-## This flag formats individual files. --files *must* be the first command line
-## arg to use this option.
-if [[ "$1" == '--files' ]]; then
-   format "${@:2}"
-   # If `--all` is passed, then any further arguments are ignored and the
-   # entire python directory is formatted.
-elif [[ "$1" == '--all' ]]; then
-   format_all
-else
-   # Format only the files that changed in last commit.
-   format_changed
-fi
-echo 'tabbyAPI yapf: Done'
-
-
-# Run Pylint
-echo 'tabbyAPI Pylint:'
-find . -name "*.py" | xargs ruff
-
-if ! git diff --quiet &>/dev/null; then
-    echo 'Reformatted files. Please review and stage the changes.'
-    echo 'Changes not staged for commit:'
-    echo
-    git --no-pager diff --name-only
-
-    exit 1
-fi
+# Call format command
+format_and_lint
+echo 'tabbyAPI ruff lint and format: Done'
