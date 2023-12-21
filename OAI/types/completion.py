5a29b75e5037db4c16a8cc31f0bcfe55ed69d7b6
AlpinDale <alpindale@gmail.com>
2023-12-21 12:03:12 +0000

    fix completion.py

--- OAI/types/completion.py
+++ OAI/types/completion.py
@@ -1,10 +1,15 @@
-from uuid import uuid4
+""" Completion API protocols """
 from time import time
-from pydantic import BaseModel, Field
 from typing import List, Optional, Union
-from OAI.types.common import LogProbs, UsageStats, CommonCompletionRequest
+from uuid import uuid4
+
+from pydantic import BaseModel, Field
+
+from OAI.types.common import CommonCompletionRequest, LogProbs, UsageStats
+
 
 class CompletionRespChoice(BaseModel):
+    """ Represents a single choice in a completion response. """
     # Index is 0 since we aren't using multiple choices
     index: int = 0
     finish_reason: str
@@ -13,10 +18,12 @@ class CompletionRespChoice(BaseModel):
 
 # Inherited from common request
 class CompletionRequest(CommonCompletionRequest):
+    """ Represents a completion request. """
     # Prompt can also contain token ids, but that's out of scope for this project.
     prompt: Union[str, List[str]]
 
 class CompletionResponse(BaseModel):
+    """ Represents a completion response. """
     id: str = Field(default_factory=lambda: f"cmpl-{uuid4().hex}")
     choices: List[CompletionRespChoice]
     created: int = Field(default_factory=lambda: int(time()))
