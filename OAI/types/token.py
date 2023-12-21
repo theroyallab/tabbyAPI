523540bd9cf5d8dc16552c3e1a2716c63b5c2a58
AlpinDale <alpindale@gmail.com>
2023-12-21 12:05:11 +0000

    fix token.py

--- OAI/types/token.py
+++ OAI/types/token.py
@@ -1,12 +1,16 @@
-from pydantic import BaseModel
+""" Tokenization types """
 from typing import List
 
+from pydantic import BaseModel
+
 class CommonTokenRequest(BaseModel):
+    """ Represents a common tokenization request. """
     add_bos_token: bool = True
     encode_special_tokens: bool = True
     decode_special_tokens: bool = True
 
     def get_params(self):
+        """ Get the parameters for tokenization. """
         return {
             "add_bos_token": self.add_bos_token,
             "encode_special_tokens": self.encode_special_tokens,
@@ -14,17 +18,22 @@ class CommonTokenRequest(BaseModel):
         }
 
 class TokenEncodeRequest(CommonTokenRequest):
+    """ Represents a tokenization request. """
     text: str
 
 class TokenEncodeResponse(BaseModel):
+    """ Represents a tokenization response. """
     tokens: List[int]
     length: int
 
 class TokenDecodeRequest(CommonTokenRequest):
+    """" Represents a detokenization request. """
     tokens: List[int]
 
 class TokenDecodeResponse(BaseModel):
+    """ Represents a detokenization response. """
     text: str
 
 class TokenCountResponse(BaseModel):
-    length: int 
+    """ Represents a token count response. """
+    length: int
