2b3e578e060b3a6a6696bbab7525b0d9ac498d0e
AlpinDale <alpindale@gmail.com>
2023-12-21 11:54:46 +0000

    fix wheel_test.py

--- tests/wheel_test.py
+++ tests/wheel_test.py
@@ -1,3 +1,4 @@
+""" Test if the wheels are installed correctly. """
 from importlib.metadata import version
 from importlib.util import find_spec
 
@@ -5,7 +6,9 @@ successful_packages = []
 errored_packages = []
 
 if find_spec("flash_attn") is not None:
-    print(f"Flash attention on version {version('flash_attn')} successfully imported")
+    print(
+        f"Flash attention on version {version('flash_attn')} successfully imported"
+    )
     successful_packages.append("flash_attn")
 else:
     print("Flash attention 2 is not found in your environment.")
@@ -32,10 +35,11 @@ else:
     print("Jinja2 is not found in your environment.")
     errored_packages.append("jinja2")
 
-print(
-    f"\nSuccessful imports: {', '.join(successful_packages)}",
-    f"\nErrored imports: {''.join(errored_packages)}"
-)
+print(f"\nSuccessful imports: {', '.join(successful_packages)}",
+      f"\nErrored imports: {''.join(errored_packages)}")
 
 if len(errored_packages) > 0:
-    print("\nIf packages are installed, but not found on this test, please check the wheel versions for the correct python version and CUDA version (if applicable).")
+    print(
+        "\nIf packages are installed, but not found on this test, please "
+        "check the wheel versions for the correct python version and CUDA version "
+        "(if applicable).")
