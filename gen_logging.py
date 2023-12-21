4a72b7b533c4f90861fe9466c178d037eefbaed4
AlpinDale <alpindale@gmail.com>
2023-12-21 10:45:56 +0000

    fix gen_logging.py

--- gen_logging.py
+++ gen_logging.py
@@ -1,35 +1,39 @@
+"""
+Functions for logging generation events.
+"""
 from typing import Dict
 from pydantic import BaseModel
 
 
-# Logging preference config
 class LogConfig(BaseModel):
+    """Logging preference config."""
     prompt: bool = False
     generation_params: bool = False
 
 
 # Global reference to logging preferences
-config = LogConfig()
+CONFIG = LogConfig()
 
 
-# Wrapper to set the logging config for generations
 def update_from_dict(options_dict: Dict[str, bool]):
-    global config
+    """Wrapper to set the logging config for generations"""
+    global CONFIG  # pylint: disable=global-statement
 
     # Force bools on the dict
     for value in options_dict.values():
         if value is None:
             value = False
 
-    config = LogConfig.model_validate(options_dict)
+    CONFIG = LogConfig.model_validate(options_dict)
 
 
 def broadcast_status():
+    """Broadcasts the current logging status"""
     enabled = []
-    if config.prompt:
+    if CONFIG.prompt:
         enabled.append("prompts")
 
-    if config.generation_params:
+    if CONFIG.generation_params:
         enabled.append("generation params")
 
     if len(enabled) > 0:
@@ -38,17 +42,19 @@ def broadcast_status():
         print("Generation logging is disabled")
 
 
-# Logs generation parameters to console
 def log_generation_params(**kwargs):
-    if config.generation_params:
+    """Logs generation parameters to console."""
+    if CONFIG.generation_params:
         print(f"Generation options: {kwargs}\n")
 
 
 def log_prompt(prompt: str):
-    if config.prompt:
+    """Logs the prompt to console."""
+    if CONFIG.prompt:
         print(f"Prompt: {prompt if prompt else 'Empty'}\n")
 
 
 def log_response(response: str):
-    if config.prompt:
+    """Logs the response to console."""
+    if CONFIG.prompt:
         print(f"Response: {response if response else 'Empty'}\n")
