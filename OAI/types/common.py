f86df6655c8b555d160e1258c9ad61474cf49b85
kingbri <bdashore3@proton.me>
2024-02-03 01:06:43 -0500

    OAI: Update logprobs type
    Some logprobs cannot exist, so make the type optional
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- OAI/types/common.py
+++ OAI/types/common.py
@@ -9,9 +9,9 @@ class LogProbs(BaseModel):
     """Represents log probabilities."""
 
     text_offset: List[int] = Field(default_factory=list)
-    token_logprobs: List[float] = Field(default_factory=list)
+    token_logprobs: List[Optional[float]] = Field(default_factory=list)
     tokens: List[str] = Field(default_factory=list)
-    top_logprobs: List[Dict[str, float]] = Field(default_factory=list)
+    top_logprobs: List[Optional[Dict[str, float]]] = Field(default_factory=list)
 
 
 class UsageStats(BaseModel):
