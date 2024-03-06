decebc08a86c89791e2199b46f366fdb3784d820
kingbri <bdashore3@proton.me>
2024-03-06 00:37:31 -0500

    Progress: Switch to Rich
    Rich is a more mature library for displaying progress bars, logging,
    and console output. This should help properly align progress bars
    within the terminal.
    
    Side note: "We're Rich!"
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- common/utils.py
+++ common/utils.py
@@ -2,6 +2,14 @@
 
 import traceback
 from pydantic import BaseModel
+from rich.progress import (
+    Progress,
+    TextColumn,
+    BarColumn,
+    TimeRemainingColumn,
+    TaskProgressColumn,
+    MofNCompleteColumn,
+)
 from typing import Optional
 
 from common.logger import init_logger
@@ -56,6 +64,18 @@ def handle_request_error(message: str, exc_info: bool = True):
 def get_sse_packet(json_data: str):
     """Get an SSE packet."""
     return f"data: {json_data}\n\n"
+
+
+def get_loading_progress_bar():
+    """Gets a pre-made progress bar for loading tasks."""
+
+    return Progress(
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TaskProgressColumn(),
+        MofNCompleteColumn(),
+        TimeRemainingColumn(),
+    )
 
 
 def unwrap(wrapped, default=None):
