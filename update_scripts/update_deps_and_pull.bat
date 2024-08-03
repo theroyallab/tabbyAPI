7ce46cc2da12fdfab81c8391027b4f6f83770bbf
kingbri <bdashore3@proton.me>
2024-08-03 13:03:24 -0400

    Start: Rewrite start scripts
    Start scripts now don't update dependencies by default due to mishandling
    caches from pip. Also add dedicated update scripts and save options
    to a JSON file instead of a text one.
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- update_scripts/update_deps_and_pull.bat
+++ update_scripts/update_deps_and_pull.bat
@@ -3,21 +3,22 @@
 :: Creates a venv if it doesn't exist and runs the start script for requirements upgrades
 :: This is intended for users who want to start the API and have everything upgraded and installed
 
-cd "%~dp0"
+:: cd to the parent directory
+cd "%~dp0.."
 
 :: Don't create a venv if a conda environment is active
 if exist "%CONDA_PREFIX%" (
     echo It looks like you're in a conda environment. Skipping venv check.
 ) else (
     if not exist "venv\" (
-        echo Venv doesn't exist! Creating one for you.
-        python -m venv venv
+        echo Venv doesn't exist! Please run start.bat instead.
+        exit 0
     )
 
     call .\venv\Scripts\activate.bat
 )
 
 :: Call the python script with batch args
-call python start.py %*
+call python start.py --update-deps --update-repository %*
 
 pause
