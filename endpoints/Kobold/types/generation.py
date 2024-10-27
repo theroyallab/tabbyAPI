8c24ee571a0ec237e48bfe247caeedd23b89bc30
kingbri <bdashore3@proton.me>
2024-10-27 11:27:12 -0400

    Kobold: Rework badwordsids
    Currently, this serves to ban the EOS token. All other functionality
    was legacy, so remove it.
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- endpoints/Kobold/types/generation.py
+++ endpoints/Kobold/types/generation.py
@@ -2,9 +2,8 @@ from functools import partial
 from pydantic import BaseModel, Field, field_validator
 from typing import List, Optional
 
-from common import model
 from common.sampling import BaseSamplerRequest, get_default_sampler_value
-from common.utils import flat_map, unwrap
+from common.utils import unwrap
 
 
 class GenerateRequest(BaseSamplerRequest):
@@ -31,26 +30,10 @@ class GenerateRequest(BaseSamplerRequest):
 
         return v
 
+    # Currently only serves to ban EOS token, but can change
     @field_validator("use_default_badwordsids", mode="before")
     def validate_badwordsids(cls, v, field_info):
-        if v:
-            bad_words_ids = []
-
-            # Try fetching badwordsids from generation config and hf config
-            if model.container.generation_config:
-                bad_words_ids += model.container.generation_config.bad_words_ids
-
-            if model.container.hf_config:
-                bad_words_ids += model.container.hf_config.get_badwordsids()
-
-            # Add badwordsids to existing banned_tokens and ban the EOS token
-            banned_tokens = unwrap(field_info.data.get("banned_tokens"), [])
-
-            if bad_words_ids:
-                field_info.data["banned_tokens"] = banned_tokens + flat_map(
-                    bad_words_ids
-                )
-                field_info.data["ban_eos_token"] = True
+        field_info.data["ban_eos_token"] = v
 
         return v
 
