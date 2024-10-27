9681e9c85f6c35a4d7e59f6b3d9cc419d799a06e
kingbri <bdashore3@proton.me>
2024-10-27 01:47:31 -0400

    Kobold: Update validators and fix parameter application
    Validators on parent fields cannot see child fields. Therefore,
    validate using the child fields instead and alter the parent field
    data from there.
    
    Also fix badwordsids casting.
    
    Signed-off-by: kingbri <bdashore3@proton.me>

--- endpoints/Kobold/types/generation.py
+++ endpoints/Kobold/types/generation.py
@@ -1,7 +1,7 @@
 from functools import partial
+from pydantic import BaseModel, Field, field_validator
 from typing import List, Optional
 
-from pydantic import BaseModel, Field, field_validator
 from common import model
 from common.sampling import BaseSamplerRequest, get_default_sampler_value
 from common.utils import flat_map, unwrap
@@ -15,36 +15,42 @@ class GenerateRequest(BaseSamplerRequest):
         default_factory=partial(get_default_sampler_value, "dynatemp_range")
     )
 
-    # Validate on the parent class's values
-    @field_validator("penalty_range")
+    # Validate on the parent class's fields
+    @field_validator("penalty_range", mode="before")
     def validate_penalty_range(cls, v):
         return -1 if v == 0 else v
 
-    @field_validator("min_temp", "max_temp")
+    @field_validator("dynatemp_range", mode="before")
     def validate_temp_range(cls, v, field_info):
-        if (
-            "dynatemp_range" in field_info.data
-            and field_info.data["dynatemp_range"] is not None
-        ):
-            temperature = unwrap(field_info.data.get("temperature"), 0)
+        if v > 0:
+            # A default temperature is always 1
+            temperature = unwrap(field_info.data.get("temperature"), 1)
 
-            if field_info.field_name == "min_temp":
-                return temperature - field_info.data["dynatemp_range"]
-            elif field_info.field_name == "max_temp":
-                return temperature + field_info.data["dynatemp_range"]
+            field_info.data["min_temp"] = temperature - v
+            field_info.data["max_temp"] = temperature + v
 
         return v
 
-    @field_validator("banned_tokens")
+    @field_validator("use_default_badwordsids", mode="before")
     def validate_badwordsids(cls, v, field_info):
-        if field_info.data.get("use_default_badwordsids"):
-            bad_words_ids = unwrap(
-                model.container.generation_config.bad_words_ids,
-                model.container.hf_config.get_badwordsids(),
-            )
+        if v:
+            bad_words_ids = []
+
+            # Try fetching badwordsids from generation config and hf config
+            if model.container.generation_config:
+                bad_words_ids += model.container.generation_config.bad_words_ids
+
+            if model.container.hf_config:
+                bad_words_ids += model.container.hf_config.get_badwordsids()
+
+            # Add badwordsids to existing banned_tokens and ban the EOS token
+            banned_tokens = unwrap(field_info.data.get("banned_tokens"), [])
 
             if bad_words_ids:
-                return v + flat_map(bad_words_ids)
+                field_info.data["banned_tokens"] = banned_tokens + flat_map(
+                    bad_words_ids
+                )
+                field_info.data["ban_eos_token"] = True
 
         return v
 
