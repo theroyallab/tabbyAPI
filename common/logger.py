357eebffd2eb1eaff276cda8491e8821b4b7461f
turboderp <11859846+turboderp@users.noreply.github.com>
2026-03-30 00:33:01 +0200

    Logger: Fix invalid escape sequence (gave syntax warning)

--- common/logger.py
+++ common/logger.py
@@ -70,7 +70,7 @@ def _log_formatter(record: dict):
     message = unwrap(record.get("message"), "")
 
     # Replace once loguru allows for turning off str.format
-    message = message.replace("{", "{{").replace("}", "}}").replace("<", "\<")
+    message = message.replace("{", "{{").replace("}", "}}").replace("<", r"\<")
 
     # Escape markup tags from Rich
     message = escape(message)
