f226d708c9b704ccbc749c1bfe1c251d8d3bd821
dr_housemd <gamerloveazino@gmail.com>
2026-08-08 17:02:13 +0300

    Update sampling.py (#441)
    * Update sampling.py
    
    Deleted xtc_probability from unsupported_params.
    
    * Update sampling.py
    
    Deleted both banned_tokens and xtc_probability from unsupported_params as they were both added.

--- common/sampling.py
+++ common/sampling.py
@@ -24,14 +24,12 @@ from common.utils import filter_none_values, unwrap
 # Requests that activate any of these get a warning and the param is ignored.
 UNSUPPORTED_PARAMS = {
     "ban_eos_token": False,
-    "banned_tokens": [],
     "allowed_tokens": [],
     "smoothing_factor": 0.0,
     "top_a": 0.0,
     "tfs": 1.0,
     "typical": 1.0,
     "skew": 0.0,
-    "xtc_probability": 0.0,
     "dry_multiplier": 0.0,
     "mirostat_mode": 0,
     "temp_exponent": 1.0,
